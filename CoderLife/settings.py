de79c5d9f43e5399d7cfbc90ef2ddc0eb4cff117
6fried <hohelric@gmail.com>
2021-07-27 03:27:53 +0100

    error 500 bug fixed

--- CoderLife/settings.py
+++ CoderLife/settings.py
@@ -88,24 +88,16 @@ WSGI_APPLICATION = 'CoderLife.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-if DEBUG:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'CoderLife',
+        'USER': 'Kelly',
+        'PASSWORD': 'Automatic133',
+        'HOST': '',
+        'PORT': '5432'
     }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql',
-            'NAME': 'CoderLife',
-            'USER': 'Kelly',
-            'PASSWORD': 'Automatic133',
-            'HOST': '',
-            'PORT': '5432'
-        }
-    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
@@ -236,5 +228,5 @@ CKEDITOR_CONFIGS = {
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# if os.environ.get('ENV') == 'PRODUCTION':
-    # django_heroku.settings(locals())
+if os.environ.get('ENV') == 'PRODUCTION':
+    django_heroku.settings(locals())
--- showcase/admin.py
+++ showcase/admin.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
-from .models import Showcase
+from .models import Project
 
-class ShowcaseAdmin(admin.ModelAdmin):
+class ProjectAdmin(admin.ModelAdmin):
     list_display = ('name', 'slug', 'created_at')
     list_filter = ('created_at',)
     search_fields = ('name', 'content')
     prepopulated_fields = {'slug': ('name',)}
     ordering = ('created_at',)
 
-admin.site.register(Showcase, ShowcaseAdmin)
+admin.site.register(Project, ProjectAdmin)
